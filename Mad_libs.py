8661c9c24066eeae7e694f67677d935683db17c2
Cao Mai <caomai@rocketmail.com>
2019-09-02 14:15:11 -0700

    Updated to more descriptive variable names

--- Mad_libs.py
+++ Mad_libs.py
@@ -1,4 +1,4 @@
-madLib_dic = {}
+madLibs_dic = {}
 
 list1 = ["Please enter a noun: ", "Please enter a verb: ", "Please enter a adjective: "]
 list2 = []
@@ -6,7 +6,7 @@ list2 = []
 for x in range(len(list1)):
     key = list1[x]
     new_value = input(list1[x])
-    madLib_dic[key] = new_value
+    madLibs_dic[key] = new_value
 
-response = f"This is a noun {madLib_dic['Please enter a noun: ']}"
-print(response)
+print_out_story =
+print(print_out_story)
