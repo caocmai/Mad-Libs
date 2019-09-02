a5df901b75d2e6d2fd61fa3c6636d885ba6ef367
Cao Mai <caomai@rocketmail.com>
2019-09-02 14:04:49 -0700

    Remove unnecessary dictionary values

--- Mad_libs.py
+++ Mad_libs.py
@@ -1,16 +1,12 @@
-"""Something."""
-madLib_dic = {
-    "noun": "blank",
-    "verb": "blank",
-    "adjective": "blank"
-}
+madLib_dic = {}
 
-list = ["noun", "verb", "adjective"]
-list2 = ["enter noun", "enter verb", "enter adjective"]
-for x in range(len(list)):
-    key = list[x]
-    new_value = input(list2[x])
+list1 = ["Please enter a noun: ", "Please enter a verb: ", "Please enter a adjective: "]
+list2 = []
+
+for x in range(len(list1)):
+    key = list1[x]
+    new_value = input(list1[x])
     madLib_dic[key] = new_value
 
-response = f"This is a noun {madLib_dic['noun']}"
+response = f"This is a noun {madLib_dic['Please enter a noun: ']}"
 print(response)
