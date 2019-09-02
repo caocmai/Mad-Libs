d6fec7f1c90a0c320538785f1a2d522c3b1a0e99
Cao Mai <caomai@rocketmail.com>
2019-09-02 14:25:38 -0700

    A full working file

--- Mad_libs.py
+++ Mad_libs.py
@@ -8,5 +8,5 @@ for x in range(len(list1)):
     new_value = input(list1[x])
     madLibs_dic[key] = new_value
 
-print_out_story =
+print_out_story = f"A {madLibs_dic['Please enter a noun: ']} is in the room. The {madLibs_dic['Please enter a noun: ']} {madLibs_dic['Please enter a verb: ']} {madLibs_dic['Please enter a adjective: ']}. "
 print(print_out_story)
