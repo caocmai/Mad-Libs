480d85836f29b1c5f223058f95acd4df979a89f7
Cao Mai <caomai@rocketmail.com>
2019-09-02 19:16:05 -0700

    Added helpful comments

--- Mad_libs.py
+++ Mad_libs.py
@@ -1,12 +1,15 @@
-madLibs_dic = {}
+# Creating empty dictionary
+mad_libs_dic = {}
 
 list1 = ["Please enter a noun: ", "Please enter a verb: ", "Please enter a adjective: "]
+# Empty list to populate with user response
 list2 = []
 
+# Looping through the list of prompts
 for x in range(len(list1)):
     key = list1[x]
-    new_value = input(list1[x])
-    madLibs_dic[key] = new_value
+    user_input = input(list1[x])
+    mad_libs_dic[key] = user_input
 
-print_out_story = f"A {madLibs_dic['Please enter a noun: ']} is in the room. The {madLibs_dic['Please enter a noun: ']} {madLibs_dic['Please enter a verb: ']} {madLibs_dic['Please enter a adjective: ']}. "
+print_out_story = f"A {mad_libs_dic['Please enter a noun: ']} is in the room. The {mad_libs_dic['Please enter a noun: ']} {mad_libs_dic['Please enter a verb: ']} {mad_libs_dic['Please enter a adjective: ']}. "
 print(print_out_story)
