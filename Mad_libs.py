e21bc65b9318489e33bf4ddd51abfb7466de0335
Cao Mai <caomai@rocketmail.com>
2019-09-03 10:18:48 -0700

    Add color to output

--- Mad_libs.py
+++ Mad_libs.py
@@ -1,7 +1,7 @@
 # Creating empty dictionary
 mad_libs_dic = {}
 
-list1 = ["Please enter a noun: ", "Please enter a verb: ", "Please enter a adjective: "]
+list1 = ["Please enter a noun: ", "Please enter a verb: ", "Please enter a adjective: ", "Please enter an adverb: "]
 # Empty list to populate with user response
 list2 = []
 
@@ -10,6 +10,11 @@ for x in range(len(list1)):
     key = list1[x]
     user_input = input(list1[x])
     mad_libs_dic[key] = user_input
+    # for letter in user_input:
+    #     if letter == '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' or '0':
+    #         print("Not valid response")
+    #     else:
+    # return letter
 
-print_out_story = f"A {mad_libs_dic['Please enter a noun: ']} is in the room. The {mad_libs_dic['Please enter a noun: ']} {mad_libs_dic['Please enter a verb: ']} {mad_libs_dic['Please enter a adjective: ']}. "
+print_out_story = f"\033[0;33;40m A {mad_libs_dic['Please enter a noun: ']} is in the room. \033[0;32;40m The {mad_libs_dic['Please enter a noun: ']} {mad_libs_dic['Please enter a verb: ']} {mad_libs_dic['Please enter a adjective: ']}. \033[0;34;40m The {mad_libs_dic['Please enter a noun: ']} {mad_libs_dic['Please enter a verb: ']} {mad_libs_dic['Please enter an adverb: ']}. "
 print(print_out_story)
